e4ec91e424de6394bba59ee151b70ab3b68f0615
dvp236 <dvp236@nyu.edu>
2016-05-21 21:47:24 -0400

    *updated addtwo problem

--- src/problems/AddTwoNumbers.java
+++ src/problems/AddTwoNumbers.java
@@ -1,5 +1,30 @@
 package problems;
 
 public class AddTwoNumbers {
+	public static ListNode addTwo(ListNode l1, ListNode l2){
+		ListNode head=null;
+		ListNode curr=head;
+		int carry = 0;
+		while(l1!=null || l2!=null || carry !=0){
+			int data=carry, vl1 = 0, vl2 = 0;
+			if(l1 != null) vl1 = l1.val;
+			if(l2 != null) vl2 = l2.val;
+			data  += vl1+vl2;
+			carry = data/10;
+			data = data % 10;
+			ListNode newNode = new ListNode(data);
+			if(head != null){
+				curr.next = newNode;
+				curr = curr.next;
+			}
+			else{
+				head = newNode;
+				curr = newNode;
+			}
+			if(l1 != null) l1 = l1.next;
+			if(l2 != null) l2 = l2.next;
+		}
+		return head;
+	}
 
 }
