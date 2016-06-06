2e5367b428602502cde0c1cfded506758fc20374
dvp236 <dvp236@nyu.edu>
2016-06-06 19:32:38 -0400

    *finished subsequence problem

--- src/problems/LongestSubString.java
+++ src/problems/LongestSubString.java
@@ -15,27 +15,33 @@ public class LongestSubString {
 	public static int getLengthOfSubstring(String s) {
         int n = s.length(), result = 0; int lo=0,hi=0;
         HashMap<Character, Integer> map = new HashMap<Character, Integer>();
-        
+        //Goal is to maintain the window of substring
+        //between i and j 
+        //[i,j] is the substring 
         for (int j = 0, i = 0; j < n; ++j) {
             if (map.containsKey(s.charAt(j))) {
+            	//we have to take max of i and map index
+            	//because the window only moves forward.
                 i = Math.max(map.get(s.charAt(j)), i);
+            	
             }
-            //if it is substring get index hi, lo
-            if(j-i > result){
+            //get hi and lo to have substring
+            if(j-i+1 > result){
             	hi = j+1;
-            	lo = i+1;
+            	lo = i;
             }
-            result = Math.max(result, j - i);
-            map.put(s.charAt(j), j);
+            //calculate substring length at each iteration 
+            result = Math.max(result, j - i+1);
+            map.put(s.charAt(j), j+1);
         }
         System.out.println(s.substring(lo,hi));
         return result;
     }
+	
 	public static void main(String[] args){
-		String st="abcdeabcdttabcdefg";
+		String st="abcdeabcdtt";
 		String st2 = "pwwkew";
-		System.out.println(getLengthOfSubstring(st));
 		System.out.println(getLengthOfSubstring(st2));
-		System.out.println(st2.substring(0,2));
+		System.out.println(getLengthOfSubstring("abbcda"));
 	}
 }
