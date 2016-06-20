27385ff305d25a7ff8d7c063eeeae8413fddb4b8
dvp236 <dvp236@nyu.edu>
2016-06-20 03:04:08 -0400

    *updated new problem

--- src/problems/LongestPalindromeSubstring.java
+++ src/problems/LongestPalindromeSubstring.java
@@ -1,5 +1,35 @@
 package problems;
 
 public class LongestPalindromeSubstring {
+	public static String getLongestPalindrome(String st){
+		String result = "";
+		for(int i=1;i<st.length()-1;i++){
+			//get the palindrome string around index i
+			String s1 = findPattern(st,i,i);
+			if(s1.length() > result.length()) result = s1;
+			String s2= findPattern(st,i,i+1);
+			if(s2.length() > result.length()) result = s2;
+		}
+		return result;
+	}
+	
+	public static String findPattern(String st, int i, int j){
+		int left  = i,right=j;
+		
+		while(left > -1 && right < st.length() && st.charAt(left) == st.charAt(right)){
+			//pass
+			left--;
+			right++;
+		}
+		
+		String found = st.substring(left+1, right);
+		return found;
+	}
+	
+	public static void main(String[] args){
+		String s = "fffabcddcbafaba";
+		System.out.println(getLongestPalindrome(s));
+		
+	}
 
 }
