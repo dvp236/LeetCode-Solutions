1519759aa5826d9fdf0614b6760698ea6dcb50e9
dvp236 <dvp236@nyu.edu>
2016-06-22 18:22:27 -0400

    *Added new problem

---
+++ src/problems/StringToInt.java
@@ -0,0 +1,16 @@
+package problems;
+import java.io.Reader;
+import java.util.NoSuchElementException;
+public class StringToInt {
+	public static int aToI(String st){
+		
+		return 0;
+	}
+	public static void main(String[] args) {
+		System.out.println(aToI("234"));
+		Reader r = new Reader();
+		r.
+		throw new NoSuchElementException();
+	}
+
+}
