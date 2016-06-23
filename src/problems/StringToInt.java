c67b5b7ccd18f6ff790227dd003b772c9bfaaffa
dvp236 <dvp236@nyu.edu>
2016-06-23 10:29:03 -0400

    *updated atoi problem

--- src/problems/StringToInt.java
+++ src/problems/StringToInt.java
@@ -1,16 +1,19 @@
 package problems;
-import java.io.Reader;
-import java.util.NoSuchElementException;
 public class StringToInt {
-	public static int aToI(String st){
+	public static int aToI(String str){
+		int result = 0;
+		int i = 0;
+		// calculate value
+		while (str.length() > i && str.charAt(i) >= '0' && str.charAt(i) <= '9') {
+			result = result * 10 + (str.charAt(i) - '0');
+			i++;
+		}
 		
-		return 0;
+		return result;
 	}
 	public static void main(String[] args) {
-		System.out.println(aToI("234"));
-		Reader r = new Reader();
-		r.
-		throw new NoSuchElementException();
+		System.out.println(aToI("2334")+1);
+		
 	}
 
 }
