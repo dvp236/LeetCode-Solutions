dae2de661629bd95867df5b774f18fb7a9bffb42
Dharmik Patel <dvp236@nyu.edu>
2016-12-01 07:09:38 -0500

    *Added LC Prefix problem

---
+++ src/problems/LongestCommonPrefix.java
@@ -0,0 +1,20 @@
+package problems;
+/**
+ * 
+ * @author dharmik
+ * 
+ * Write a function to find the longest common prefix string amongst an array of strings.
+ *
+ */
+public class LongestCommonPrefix {
+	
+	public static String longestCommonPrefix(String[] strs){
+		return "";
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
