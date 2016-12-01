2e07ff8ed95736b0f7b4ffbcf8db0abac4d8194e
Dharmik Patel <dvp236@nyu.edu>
2016-12-01 06:07:40 -0500

    *completed container problem

--- src/problems/ContainerWithMostWater.java
+++ src/problems/ContainerWithMostWater.java
@@ -10,9 +10,38 @@ package problems;
  *
  */
 public class ContainerWithMostWater {
-
+	
+	//Bruteforce 
+	//time complexity: O(n^2)
+	public static int maxArea1(int[] heights){
+		int area = 0;
+		for(int i=0; i<heights.length;i++){
+			for(int j=i+1; j< heights.length;j++){
+				area = Math.max(area, Math.abs(Math.min(heights[i], heights[j])*(j-i)));
+			}
+		}
+		return area;
+		
+	}
+	
+	//go from both the side evaluating area
+	//to maximize area, move from the side where height is smaller
+	//thus trying to keep are maximum between the lines
+	//Time compelxity : O(n)
+	public static int maxArea(int[] heights){
+		int area = 0;
+		
+		for(int i=0, j=heights.length-1 ; i<j ;){
+			area = Math.max(area, Math.abs(Math.min(heights[i], heights[j])*(j-i)));
+			//move from the smaller side, keep max height 
+			if(heights[i] > heights[j]) j--;
+			else i++;
+		}
+		
+		return area;
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
