83717a2da0afd9bfca9c4796e1e637752570d1ea
Dharmik Patel <dvp236@nyu.edu>
2016-12-08 17:07:15 -0500

    *update to LCP

--- src/problems/LongestCommonPrefix.java
+++ src/problems/LongestCommonPrefix.java
@@ -9,12 +9,32 @@ package problems;
 public class LongestCommonPrefix {
 	
 	public static String longestCommonPrefix(String[] strs){
-		return "";
+		String result = "";
+		
+		outerloop:
+		for(int i=0;true ;i++){
+			//loop over char
+			char c =' ';
+			for(int j=0; j<strs.length;j++){
+				if(strs[j].length() <= i) break outerloop;
+				char curr = strs[0].charAt(i);
+				c = strs[j].charAt(i);
+				
+				if(c != curr ) break outerloop;
+				
+				
+			}
+			result +=c;
+			
+		}
+		
+		return result;
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String[] strs = new String[]{"abclm","abcd","abcdef"};
+		System.out.println(longestCommonPrefix(strs));
 	}
 
 }
