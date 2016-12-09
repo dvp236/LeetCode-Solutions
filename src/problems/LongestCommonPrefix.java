436dc63c7872585e9e87187253304fc835e805ab
Dharmik Patel <dvp236@nyu.edu>
2016-12-09 16:59:11 -0500

    *updated LCP problem

--- src/problems/LongestCommonPrefix.java
+++ src/problems/LongestCommonPrefix.java
@@ -8,11 +8,14 @@ package problems;
  */
 public class LongestCommonPrefix {
 	
-	public static String longestCommonPrefix(String[] strs){
+	
+	//Vertical Scanning of strings
+	//Complexity: O(S) where s is all characters in array of strings 
+	public static String longestCommonPrefixOne(String[] strs){
 		String result = "";
 		
 		outerloop:
-		for(int i=0;true ;i++){
+		for(int i=0;i<strs[0].length();i++){
 			//loop over char
 			char c =' ';
 			for(int j=0; j<strs.length;j++){
@@ -30,10 +33,39 @@ public class LongestCommonPrefix {
 		
 		return result;
 	}
-
+	public static String longestCommonPrefix(String[] strs){
+		if(strs == null || strs.length == 0) return "";
+		if(strs.length ==1) return strs[0];
+		return longestCommonPrefixDAC(strs,0,strs.length-1);
+	}
+	//Divide and conquer approach
+	public static String longestCommonPrefixDAC(String[] strs,int start,int end){
+		if(start==end) return strs[start];
+		else{
+			int mid = (start+end)/2;
+			String common1 = longestCommonPrefixDAC(strs,start,mid);
+			String common2 = longestCommonPrefixDAC(strs,mid+1,end);
+			return lcpHelper(common1,common2);
+			
+		}
+	}
+	
+	//helper method to get lcp of two string
+	private static String lcpHelper(String s1,String s2){
+		int min=s1.length() <= s2.length() ? s1.length() : s2.length();
+		for(int i=0; i<min; i++){
+			if(s1.charAt(i)!=s2.charAt(i))
+				return s1.substring(0, i);
+			
+		}
+		return s1.substring(0,min);
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String[] strs = new String[]{"abclm","abcd","abcdef"};
+		String[] strs = new String[]{"abclm","abcd","abcdef","ab","abcd"};
+		System.out.println(longestCommonPrefixOne(strs));
+		
 		System.out.println(longestCommonPrefix(strs));
 	}
 
