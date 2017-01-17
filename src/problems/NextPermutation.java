cab62b1a903fdf606e2621250a85a6c4211e2819
Dharmik Patel <dvp236@nyu.edu>
2017-01-17 14:37:16 -0500

    *added new problem

--- src/problems/NextPermutation.java
+++ src/problems/NextPermutation.java
@@ -1,10 +1,59 @@
 package problems;
 
+import java.util.Arrays;
+
+/**
+ * 
+ * @author dharmik
+ * Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
+ * If such arrangement is not possible, it must rearrange it as the lowest possible order (ie, sorted in ascending order).
+ * The replacement must be in-place, do not allocate extra memory.
+ * Here are some examples. Inputs are in the left-hand column and its corresponding outputs are in the right-hand column.
+ * 1,2,3 -> 1,3,2
+ * 3,2,1 -> 1,2,3
+ * 1,1,5 -> 1,5,1
+ * 
+ * 
+ */
 public class NextPermutation {
 
+	//take 2 pass. First find the value to replace 
+	//find just bigger value for replacement
+	//reverse remaining elements 
+	public static int[] nextPermutation(int[] a){
+		int k=a.length-2,j;
+		
+		while(k>=0 && a[k]>=a[k+1]) k--;
+		
+		if(k>=0){
+			j=a.length-1;
+			while(j>=0 && a[j] <= a[k]) j--;
+			swap(a,k,j);
+		}
+		
+		reverse(a, k+1, a.length-1);
+		return a;
+	}
+	
+	
+	public static void swap(int[] a, int i, int j){
+		int temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+	
+	public static void reverse(int[] a, int i, int j){
+		while(i<j){
+			swap(a,i,j);
+			i++;
+			j--;
+		}
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//System.out.println("gajab");
+		int[] a = new int[]{1,5,1};
+		System.out.println(Arrays.toString(a));
+		System.out.println(Arrays.toString(nextPermutation(a)));
 	}
 
 }
