625d83591b0bc307157e34f9b60a27aa076397ab
Dharmik Patel <dvp236@nyu.edu>
2017-05-14 18:16:01 -0400

    updated MinPathSum

--- src/problems/MinimumPathSum.java
+++ src/problems/MinimumPathSum.java
@@ -11,11 +11,34 @@ package problems;
 
 public class MinimumPathSum {
 	
-	public static int getMinPathSum(int[] a){
-		return 0;
+	public static int getMinPathSum(int[][] grid){
+		int m = grid.length; //num of rows
+		int n = grid[0].length; //nums of columns 
+		
+		for(int i = 1; i < n ; i++)
+			grid[0][i] += grid[0][i-1];
+		
+		for(int i = 1; i < m ; i++)
+			grid[i][0] += grid[i-1][0];
+		
+		for(int i = 1; i < m; i++) {
+			for(int j = 1; j < n; j++) {
+				grid[i][j] += Math.min(grid[i-1][j], grid[i][j-1]);				
+			}
+		}
+		
+		return grid[m-1][n-1];
 	}
 	
 	public static void main(String[] args) {
+		int[][] a = new int[][]{
+				new int[]{1,2,3,4},
+				new int[]{5,6,7,8},
+				new int[]{1,8,9,8},
+				new int[]{1,1,1,1}				
+			};
+			
+		System.out.println(getMinPathSum(a));
 		
 	}
 }
