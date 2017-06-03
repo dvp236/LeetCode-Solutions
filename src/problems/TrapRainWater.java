cb2d239699a879456efd5ada19ce87838fb3c529
Dharmik Patel <dvp236@nyu.edu>
2017-06-03 13:16:52 -0400

    updated TrapRainWater

--- src/problems/TrapRainWater.java
+++ src/problems/TrapRainWater.java
@@ -17,28 +17,36 @@ public class TrapRainWater {
 		
 		//0 1 0 2 1 0 3 2 1 2 1
 		
-		int left_max = 0; int right_max = 0;
-		int left = 0 ; int right = a.length-1;
+		int left_max = 0, right_max = 0;
+		int left = 0, right = a.length-1;
 		int ans =0;
+		//we only move ahead from smaller side.
+		//so if a[right] is smaller then a[left], we move from right side and calculate water we can trap
 		while(left < right) {
+			
 			if(a[left] < a[right]){
+				
 				if(a[left] > left_max) {
 					left_max = a[left];
 				}
 				else{
-					ans+=left_max - a[left] ;
+					ans += left_max - a[left] ;
 				}
+				
 				left++;
+				
 			}else{
+				
 				if(a[right]>right_max)
 					right_max = a[right];
 				else
-					ans+=right_max-a[right];
+					ans += right_max-a[right];
+				
 				right--;
-			}
-			
+			}	
 			
 		}
+		
 		return ans;
 		
 		
@@ -47,6 +55,8 @@ public class TrapRainWater {
 	public static void main(String[] args) {
 		int[] a = new int[]{0,1,0,2,1,0,1,3,2,1,2,1};
 		System.out.println(trap(a));
+		int[] b = new int[]{0,0,2,0,0,0,1};
+		System.out.println(trap(b));
 	}
 
 }
