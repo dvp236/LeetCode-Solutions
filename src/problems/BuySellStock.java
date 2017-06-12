ddb559e15d219a316be6427a5b0817842c00dae4
Dharmik Patel <dvp236@nyu.edu>
2017-06-11 20:40:32 -0400

    updated buy sell problem

--- src/problems/BuySellStock.java
+++ src/problems/BuySellStock.java
@@ -1,5 +1,32 @@
 package problems;
 
 public class BuySellStock {
-
+	
+	public static int maxProfit(int[] prices) {
+		int profit = 0;
+		int min = prices[0];
+		int max = prices[0];
+		
+		for(int i = 0 ; i < prices.length; i++ ) {
+			
+			if(prices[i] < min) {
+				min = prices[i];
+				max = prices[i];
+			}
+			if (prices[i] > max){
+				max = prices[i];
+				profit = Math.max(profit, max -min);
+			}
+		}
+		
+		
+        return profit;
+    }
+	
+	public static void main(String[] args) {
+		System.out.println("BuySell Leetcode");
+		int[] prices = new int[]{3,4,7,2,5,9,1,7};
+		System.out.println(maxProfit(prices));
+	}
+	
 }
